db6f66c8af1528cdda302330eaf5e9572d24ce28
Karthik Rao <karthik.rao543@gmail.com>
2020-05-14 22:26:55 -0400

    fixed broken links from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Link to="/blog/ml/speedChallenge/1">Speed Challenge 1</Link>
-    <Link to="/blog/ml/cudaInstall/1">How to install Cuda 10.1 on Ubuntu 20.04</Link>
+    <Link to="/ml/speedChallenge/1">Speed Challenge 1</Link>
+    <Link to="/ml/cudaInstall/1">How to install Cuda 10.1 on Ubuntu 20.04</Link>
   </Layout>
 )
 
